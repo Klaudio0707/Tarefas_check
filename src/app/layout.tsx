9b6ee47d6bdfda56f5ba9b704b45b4e694c50647
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-03-20 23:37:52 -0300

    styles da pg home e responsividade. Mais a criação do header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+      <Header/> {/* header */}
+           {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,14 @@ export default function Home() {
           Sistema Feito para voce organizar <br/>
           seus estudos e tarefas
         </h1>
+          <div className={styles.infoContent}>
+            <section className={styles.box}>
+               <span>+12 posts</span>
+            </section>
+            <section className={styles.box}>
+               <span>+90 comentarios</span>
+            </section>
+          </div>
 
      </main>
     </div>
